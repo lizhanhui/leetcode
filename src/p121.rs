e9fa48c60b365cdc735ec37a7127d1c3be1798f6
lizhanhui <lizhanhui@gmail.com>
2023-12-10 16:28:27 +0800

    optimize solution to problem 121
    Signed-off-by: lizhanhui <lizhanhui@gmail.com>

--- src/p121.rs
+++ src/p121.rs
@@ -3,35 +3,12 @@ struct Solution;
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
         let len = prices.len();
-        if len <= 1 {
-            return 0;
-        }
-
-        let mut min = prices[0];
-        let mut max = prices[len - 1];
-        let mut s_p = vec![];
-        let mut b_p = vec![];
-
-        for i in 0..len - 1 {
-            if prices[i] < min {
-                min = prices[i];
-            }
-            b_p.push(min);
-
-            if prices[len - 1 - i] > max {
-                max = prices[len - 1 - i];
-            }
-            s_p.push(max);
-        }
-
-        s_p.reverse();
-
+        // lowest price we know
+        let mut l = prices[0];
         let mut max = 0;
-        for i in 0..len - 1 {
-            let profit = s_p[i] - b_p[i];
-            if profit > max {
-                max = profit;
-            }
+        for i in 1..len {
+            l = l.min(prices[i]);
+            max = max.max(prices[i] - l);
         }
         max
     }
